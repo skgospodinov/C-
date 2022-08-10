9384e02fd05ad3f2ace79616635f537acae2a7a7
Stoian Gospodinov <skgospodinov@gmail.com>
2022-08-10 12:43:10 +0300

    added SymbolMatrix and PrimaryDiagonal

---
+++ 0.2Lab Multidimensional Arrays/3. Primary Diagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int row = i; row <= i; row++)
+                {
+                    for (int col = i; col <= i; col++)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(sum);            
+        }
+    }
+}
---
+++ 0.2Lab Multidimensional Arrays/4. Symbol in Matrix/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _4._Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            bool isConsist = false;
+            int indexRow = 0;
+            int indexCol = 0;
+            char symbol = Console.ReadLine()[0];
+
+            for (int row = 0; row < n; row++)
+            {
+                if (isConsist)
+                {
+                    break;
+                }
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        isConsist = true;
+                        indexRow = row;
+                        indexCol = col;
+                        break;
+                    }                 
+                }
+            }
+
+            if (isConsist)
+            {
+                Console.WriteLine($"({indexRow},{indexCol})");
+            }
+            else
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix ");
+            }
+        }
+    }
+}
